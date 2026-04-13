7804395b2b94889f4af3a1ad7b6a8939d52504de
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-13 22:52:19 +0530

    Add files via upload

---
+++ sayhello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sayhello {
+    public static void main(String[] args) {
+
+
+    Scanner sc=new Scanner(System.in);
+
+    String name=sc.nextLine();
+    sc.close();
+        System.out.println("Hello", + name);
+
+
+
+}}
