8ba549af746c14e6451d2d06693d826b3d12fe90
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-22 23:43:28 +0530

    Add files via upload

---
+++ thebrothers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class thebrothers {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         String f1=sc.next();
+         String s1=sc.next();
+
+         String f2=sc.next();
+         String s2=sc.next();
+
+         if(s1==s2){
+             System.out.println("ARE BROTHERS");
+         }
+         else{
+             System.out.println("NOT");
+         }
+
+
+
+
+
+    }
+}
