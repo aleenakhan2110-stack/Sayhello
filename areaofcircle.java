ca7c219f2447e8e96ef0a318f5a0e8f34e9b4ece
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-22 23:37:24 +0530

    Add files via upload

---
+++ areaofcircle.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class areaofcircle {
+     public static void main(String[] args) {
+
+
+
+    Scanner sc=new Scanner(System.in);
+         System.out.println("enter the radius");
+
+    float r=sc.nextFloat();
+    double pi=3.141592653;
+    double area=pi*r*r;
+         System.out.println(area);
+
+
+
+
+}}
