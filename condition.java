ee1a5a06c4b7ec0a2a86dcd9b0800b0f36cf19a0
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-22 23:39:41 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class condition {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int A=sc.nextInt();
+         int B=sc.nextInt();
+         if(A>B){
+             System.out.println("Yes");
+         }
+         else{
+             System.out.println("No");
+         }
+
+    }
+}
