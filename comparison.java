19a1186e3728316b54729212af6dfbcefbfb1de7
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-22 23:50:46 +0530

    Add files via upload

---
+++ comparison.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class comparison {
+   public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int A=sc.nextInt();
+       int B=sc.nextInt();
+       int S=sc.next().charAt(0);
+
+       if(S=='>'){
+           if(A>B){
+               System.out.println("Right");
+
+           }
+           else{
+               System.out.println("Wrong");
+           }
+
+       }
+       else if(S=='<'){
+           if(A<B){
+               System.out.println("Right");
+
+           }
+           else{
+               System.out.println("Wrong");
+           }
+
+       }
+       if(S=='='){
+           if(A==B){
+               System.out.println("Right");
+
+           }
+           else{
+               System.out.println("Wrong");
+           }
+
+       }
+
+
+
+   }
+
+}
